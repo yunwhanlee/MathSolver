3317b88a09b0ad3838f6e2ea85727fb3dc9b1610
DESKTOP-4QECMS1\lee <yunwhan-lee@sims.co.jp>
2023-06-05 18:43:40 +0900

    Pet 追いかける Clamp

--- Assets/Scripts/Pet.cs
+++ Assets/Scripts/Pet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Pet : MonoBehaviour {
     const int FRONT_Z = -1;
     const float CHASE_DELAY = 0.5f;
+    const float MIN_X = -2.5f, MAX_X = 2.5f, MIN_Y = -3.5f, MAX_Y = 2;
     Transform tf;
     Transform plTf;
     Vector2 targetPos;
@@ -21,8 +22,10 @@ public class Pet : MonoBehaviour {
         sr = GetComponent<SpriteRenderer>();
         sr.sortingOrder = Mathf.RoundToInt(tf.position.y) * REVERSE_Y;
 
-        //* プレイヤー隣
-        targetPos = new Vector2(plTf.position.x + 1, plTf.position.y - 0.5f);
+        //* プレイヤー 追いかける
+        float x = Mathf.Clamp((plTf.position.x + 1), MIN_X, MAX_X);
+        float y = Mathf.Clamp((plTf.position.y - 0.5f), MIN_Y, MAX_Y);
+        targetPos = new Vector2(x, y);
 
         if(targetPos.x != tf.position.x || targetPos.y != tf.position.y) {
             tf.position = Vector2.Lerp(tf.position, targetPos, moveSpeed * Time.deltaTime);
