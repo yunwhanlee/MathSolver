264df49e2b707a5e0ada57928e55d117991852b0
yunwhanlee <yunwhanlee@gmail.com>
2023-08-29 01:43:08 +0900

    Home And Minigame Scene ずれているフォント整理。

--- Assets/Scripts/public/Config.cs
+++ Assets/Scripts/public/Config.cs
@@ -22,24 +22,24 @@ public static class Config {
 
     public readonly static string MINIGAME1_TITLE = "Catch falling apples!";
     public readonly static string MINIGAME1_CONTENT = "Collect as many apples as you can.";
-    public readonly static int MINIGAME1_MAX_VAL = 200;
-    public readonly static int[] MINIGAME1_UNLOCK_SCORES = new int[3] {50, 120, MINIGAME1_MAX_VAL};
+    public readonly static int MINIGAME1_MAX_VAL = 130;
+    public readonly static int[] MINIGAME1_UNLOCK_SCORES = new int[3] {30, 80, MINIGAME1_MAX_VAL};
     public readonly static int[] MINIGAME1_EASY_OBJ_DATA = new int[2] {1, 2};
     public readonly static int[] MINIGAME1_NORMAL_OBJ_DATA = new int[3] {2, 4, 8};
     public readonly static int[] MINIGAME1_HARD_OBJ_DATA = new int[3] {3, 6, 10};
 
     public readonly static string MINIGAME2_TITLE = "Jump to the sky!";
     public readonly static string MINIGAME2_CONTENT = "Collect bananas without falling off.";
-    public readonly static int MINIGAME2_MAX_VAL = 200;
-    public readonly static int[] MINIGAME2_UNLOCK_SCORES = new int[3] {50, 120, MINIGAME1_MAX_VAL};
+    public readonly static int MINIGAME2_MAX_VAL = 130;
+    public readonly static int[] MINIGAME2_UNLOCK_SCORES = new int[3] {30, 80, MINIGAME1_MAX_VAL};
     public readonly static int[] MINIGAME2_EASY_OBJ_DATA = new int[2] {1, 3};
     public readonly static int[] MINIGAME2_NORMAL_OBJ_DATA = new int[3] {2, 5, 9};
     public readonly static int[] MINIGAME2_HARD_OBJ_DATA = new int[3] {3, 7, 10};
 
     public readonly static string MINIGAME3_TITLE = "Snow sledding!";
     public readonly static string MINIGAME3_CONTENT = "Collect blueberries avoiding obstacles.";
-    public readonly static int MINIGAME3_MAX_VAL = 200;
-    public readonly static int[] MINIGAME3_UNLOCK_SCORES = new int[3] {50, 120, MINIGAME3_MAX_VAL};
+    public readonly static int MINIGAME3_MAX_VAL = 130;
+    public readonly static int[] MINIGAME3_UNLOCK_SCORES = new int[3] {30, 80, MINIGAME3_MAX_VAL};
     public readonly static int[] MINIGAME3_EASY_OBJ_DATA = new int[2] {1, 2};
     public readonly static int[] MINIGAME3_NORMAL_OBJ_DATA = new int[3] {2, 4, 8};
     public readonly static int[] MINIGAME3_HARD_OBJ_DATA = new int[3] {3, 6, 10};
